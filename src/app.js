ca03338ab874fca1653b13791f1600dbb45d2e9f
Ishan <ishan1608@gmail.com>
2018-12-31 14:53:19 +0530

    tried using hjs-webpack documentation to get things working. Failed. Will remove hjs-webpack and everything related to it and set my own webpack next

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+var React = require('react');
+
+var Hello = React.createClass({
+	displayName: 'Hello',
+	render: function () {
+		return <div>Hello, {this.props.name}</div>;
+	}
+});
+
+React.render(<Hello name="World!"/>, document.body);
