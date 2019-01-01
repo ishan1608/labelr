be3eda7a14edf7a5f25d9d66f274a4d7e8fbc58f
Ishan <ishan1608@gmail.com>
2019-01-01 22:35:06 +0530

    added babel-loader for js files with presets as ['@babel/preset-env', '@babel/preset-react']

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,19 @@ module.exports = {
 	output: {
 		filename: 'index-bundle.js',
 		path: BUILD_DIR
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react']
+					}
+				}
+			}
+		]
 	}
 };
