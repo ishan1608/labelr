d96eca51a48e2281ec4997104246328ffffeebe9
Ishan <ishan1608@gmail.com>
2019-01-01 22:56:29 +0530

    removed lodash as a direct dependecy

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-import _ from 'lodash';
 import React from "react";
 import ReactDOM from 'react-dom';
-
-function component() {
-	let element = document.createElement('div');
-
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-	return element;
-}
-
-document.body.appendChild(component());
 
 class Hello extends React.Component {
 	render() {
