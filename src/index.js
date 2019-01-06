2341293ced0e2aadd35bafa25aed0bf8cec9801c
Ishan <ishan1608@gmail.com>
2019-01-06 14:27:52 +0530

    Installed react-hot-loader Surprisingly didn't need 1) 'react-hot-loader/patch',  // RHL patch OR 2) plugins: ['react-hot-loader/babel']
    Even more to the surpise when adding 2) above, the module was re-loaded twice

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {AppContainer} from 'react-hot-loader';
+
 import Root from './components/root';
 
-ReactDOM.render(<Root name="Webpack!"/>, document.getElementById('root'));
+const render = Component => {
+	ReactDOM.render(
+		<AppContainer>
+			<Component name="Webpack!"/>
+		</AppContainer>,
+		document.getElementById('root')
+	);
+};
+
+render(Root);
+
+if (module.hot) {
+	module.hot.accept('./components/root', () => {
+		const NextRoot = require('./components/root').default;
+		render(NextRoot);
+	});
+}
