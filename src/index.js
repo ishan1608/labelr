7a028e08d54f7e3b7378ac5157a215ad28f401d9
Ishan <ishan1608@gmail.com>
2019-01-06 15:12:42 +0530

    minor re-factoring

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import {AppContainer} from 'react-hot-loader';
 
-import Root from './components/root';
+import RootComponent from './components/root';
 
-const render = Component => {
+const renderRootComponent = (AppComponent) => {
 	ReactDOM.render(
 		<AppContainer>
-			<Component name="Webpack!"/>
+			<AppComponent name="Webpack!"/>
 		</AppContainer>,
 		document.getElementById('root')
 	);
 };
 
-render(Root);
+// initial render
+renderRootComponent(RootComponent);
 
 if (module.hot) {
+	// hot module reloading for RootComponent
 	module.hot.accept('./components/root', () => {
-		const NextRoot = require('./components/root').default;
-		render(NextRoot);
+		const NextRootComponent = require('./components/root').default;
+		renderRootComponent(NextRootComponent);
 	});
 }
