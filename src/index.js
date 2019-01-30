c3aa908e74d2795c885f3ac734c5f852f688475c
Ishan <ishan.prasad@myntra.com>
2019-01-30 11:20:09 +0530

    using the hot module provided by react-hot-loader

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {AppContainer} from 'react-hot-loader';
 import 'bootstrap';
 import './styles.scss';  // const styles = require('./styles.scss'); <-- Also works
 
 import RootComponent from './components/root/root_component';
 
-const renderRootComponent = (AppComponent) => {
-	ReactDOM.render(
-		<AppContainer>
-			<AppComponent name="Webpack!"/>
-		</AppContainer>,
-		document.getElementById('root')
-	);
-};
-
-// initial render
-renderRootComponent(RootComponent);
-
-if (module.hot) {
-	// hot module reloading for RootComponent
-	module.hot.accept(RootComponent, () => {
-		const NextRootComponent = RootComponent;
-		renderRootComponent(NextRootComponent);
-	});
-}
+ReactDOM.render(
+	<RootComponent name="Webpack!"/>,
+	document.getElementById('root')
+);
