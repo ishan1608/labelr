0696ce5fb6e648914d59a392d0500a5bccfc710b
Ishan <ishan1608@gmail.com>
2019-01-30 22:34:43 +0530

    added LikeCounter to repos page

--- src/pages/repos_page.js
+++ src/pages/repos_page.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import {hot} from "react-hot-loader/root";
 
+import LikeCounterComponent from "../components/like_counter/like_counter_component";
+
+
 class ReposPageComponent extends React.Component {
 
 	render() {
 		console.log('%c ReposPageComponent.render()', 'color: green');
-		return <div><h1>Repos Page</h1></div>;
+		return (
+			<div>
+				<h1>Repos Page</h1>
+				<LikeCounterComponent/>
+			</div>
+		);
 	}
 
 	componentWillUnmount() {
