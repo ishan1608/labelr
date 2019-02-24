518520041b69cdaf94318e14424297272dd1aaeb
Ishan <ishan1608@gmail.com>
2019-02-24 13:16:47 +0530

    Added url to Repo and added a function to get repo by full_name from the collection

--- src/models/repo-collection.js
+++ src/models/repo-collection.js
@@ -1,3 +1,4 @@
+import app from 'ampersand-app';
 import Collection from 'ampersand-rest-collection';
 
 import githubMixin from '../helpers/github-mixin';
@@ -7,5 +8,21 @@ import Repo from './repo';
 export default Collection.extend({
 	url: 'https://api.github.com/user/repos',
 
-	model: Repo
+	model: Repo,
+
+	getByFullName(fullName) {
+		let model = app.me.repos.findWhere({
+			full_name: fullName
+		});
+
+		if (!model) {
+			model = new Repo({
+				full_name: fullName
+			});
+
+			// This is asynchronous and the data will not be available right away
+			model.fetch();
+		}
+		return model;
+	}
 }, githubMixin);
