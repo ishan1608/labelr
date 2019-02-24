a157841801dea00276d8203b2e72f7a03a44bcad
Ishan <ishan1608@gmail.com>
2019-02-24 14:42:41 +0530

    Moved all the hot module replacement related config to development config leaving only the common config in common file

--- config/webpack.config.development.js
+++ config/webpack.config.development.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.config.common');
 const path = require('path');
@@ -8,9 +9,24 @@ module.exports = merge(common, {
 	mode: 'development',
 	devtool: 'inline-source-map',  // eval-cheap-module-source-map <-- another option
 
+	entry: [
+		'webpack/hot/only-dev-server',  // HMR in the browser
+		'react-hot-loader/patch', // RHL patch
+	],
+
 	devServer: {
 		hot: true,  // Enable HMR
 		contentBase: APP_DIR,  // Content Base --> APP_DIR
 		historyApiFallback: true,
-	}
+	},
+
+	resolve: {
+		alias: {
+			'react-dom': '@hot-loader/react-dom'
+		}
+	},
+
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),  // Generates hot update chunks
+	],
 });
